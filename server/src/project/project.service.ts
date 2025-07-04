e6b92a906efd897fcb16ac85a970567b31b93cef
geym-eugene <geym.eugene@gmail.com>
2025-07-04 11:16:04 +0300

    update, delete, swagger

--- server/src/project/project.service.ts
+++ server/src/project/project.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Project } from './entity';
+import { CreateProjectDto } from './dto/createProject.dto';
+import { UpdateProjectDto } from './dto/updateProject.dto';
 
 @Injectable()
 export class ProjectService {
@@ -14,8 +16,23 @@ export class ProjectService {
     return this.projectRepo.find();
   }
 
-  create(data: Partial<Project>): Promise<Project> {
-    const project = this.projectRepo.create(data);
+  create(dto: CreateProjectDto): Promise<Project> {
+    const project = this.projectRepo.create(dto);
     return this.projectRepo.save(project);
+  }
+
+  async update(id: number, dto: UpdateProjectDto): Promise<Project> {
+    const project = await this.projectRepo.findOneBy({ id });
+    if (!project) throw new NotFoundException('Проект не найден');
+
+    Object.assign(project, dto);
+    return this.projectRepo.save(project);
+  }
+
+  async remove(id: number): Promise<void> {
+    const project = await this.projectRepo.delete(id);
+    if (project.affected === 0) {
+      throw new NotFoundException('Проект не найден');
+    }
   }
 }
