b69f231dfa3bda5ee44ba282e336be54135a1a61
ZuzyaNeighbourhood <netslovanetu@mail.ru>
2025-07-10 18:16:49 +0300

    FiltersNoProblems

--- client/src/entities/projects/model/projectType.ts
+++ client/src/entities/projects/model/projectType.ts
@@ -1,46 +1,55 @@
 export interface ProjectT {
-  id: number;
-  title: string;
-  description: string;
-  area_m2: number;
-  floors: number;
-  material: string;
-  roof: string | null;
-  style: string | null;
-  price: number;
-  image_preview: string;
-  model_3d_url: string;
-  plan_pdf_url: string;
-  userId: number | null;
+    id: number;
+    title: string;
+    description: string;
+    area_m2: number;
+    floors: number;
+    material: string;
+    roof: string | null;
+    style: string | null;
+    price: number;
+    image_preview: string;
+    model_3d_url: string;
+    plan_pdf_url: string;
+    userId: number | null;
 }
 
 export type ProjectsT = ProjectT[];
 
 export type NewProjectT = {
-  title: string;
-  description: string;
-  area_m2: number;
-  floors: number;
-  material: string;
-  price: number;
-  image_preview: string;
-  model_3d_url: string;
-  plan_pdf_url: string;
+    title: string;
+    description: string;
+    area_m2: number;
+    floors: number;
+    material: string;
+    price: number;
+    image_preview: string;
+    model_3d_url: string;
+    plan_pdf_url: string;
 };
 
 export type objectFav = {
-  id: number;
-  project: ProjectT;
+    id: number;
+    project: ProjectT;
 };
 
 export type InitialStateT = {
-  projects: ProjectsT;
-  projectsFiltered: ProjectsT;
-  loading: boolean;
-  error: string | null;
-  isAddModalOpen: boolean;
-  isSecondModalOpen: boolean;
-  selectedProjectId: number | null;
-  showProjectState: boolean;
-  rangeFilter: number;
+    projects: ProjectsT;
+    projectsFiltered: ProjectsT;
+    loading: boolean;
+    error: string | null;
+    isAddModalOpen: boolean;
+    isSecondModalOpen: boolean;
+    selectedProjectId: number | null;
+    showProjectState: boolean;
+    rangeFilter: number;
+    filters: filtersStatesT
 };
+
+export type filtersStatesT = {
+    area_m2: { min: number, max: number } | null,
+    price: number | null,
+    material: string | null,
+    style: string | null,
+    floors: number | null,
+}
